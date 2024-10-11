832f45483adc80475ad2c54626dbac5f87eb9bee
alexandernovadev <alexsk88.dev@gmail.com>
2024-10-10 21:17:56 -0500

    Alex #12893 Init Tailwind

---
+++ front/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode: 'class', // O usa 'media' si prefieres usar la preferencia del sistema
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
