2d2f5beba4209fb945e6963d3e62925ab5567a5d
Alexander Nova <Alexander.Nova@Contractor.Prometric.com>
2024-10-13 17:26:45 -0500

    Tailwind.config to extend color palette

--- front/tailwind.config.js
+++ front/tailwind.config.js
@@ -1,12 +1,39 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  darkMode: 'class', // O usa 'media' si prefieres usar la preferencia del sistema
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  darkMode: "class",
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        black: {
+          100: "#F5F5F5",
+          200: "#E0E0E0",
+          300: "#CCCCCC",
+          400: "#999999",
+          500: "#666666",
+          600: "#4D4D4D",
+          700: "#333333",
+          800: "#1A1A1A",
+          900: "#0D0D0D",
+        },
+        green: {
+          100: "#B9F6CA",
+          500: "#00E676",
+          700: "#00C853",
+        },
+        gray: {
+          100: "#F5F5F5",
+          200: "#EEEEEE",
+          300: "#E0E0E0",
+          400: "#BDBDBD",
+          500: "#9E9E9E",
+          600: "#757575",
+          700: "#616161",
+          800: "#424242",
+          900: "#212121",
+        },
+      },
+    },
   },
   plugins: [],
-}
+};
