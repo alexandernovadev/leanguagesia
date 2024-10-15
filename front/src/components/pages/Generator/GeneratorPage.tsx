7d2e00d53850da0c1f1dc6d45d5dc694b3263857
Alexander Nova <Alexander.Nova@Contractor.Prometric.com>
2024-10-15 14:28:38 -0500

    Refactor GeneratorPage component to add animated send button

--- front/src/components/pages/Generator/GeneratorPage.tsx
+++ front/src/components/pages/Generator/GeneratorPage.tsx
@@ -1,5 +1,16 @@
 import { MainLayout } from "../../shared/Layouts/MainLayout";
+import "../../../styles/buttonanimatiocss.css";
+import { SendHorizontal } from "lucide-react";
+
 
 export const GeneratorPage = () => {
-  return <MainLayout>GeneratorPage</MainLayout>;
+  return (
+    <MainLayout>
+      <button className="box m-4">
+        <span><SendHorizontal /></span>
+      </button>
+    </MainLayout>
+  );
 };
+
+
