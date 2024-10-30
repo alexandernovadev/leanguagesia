54c6281d926f62fed47748d6a51d334b4413f46f
alexandernovadev <alexsk88.dev@gmail.com>
2024-10-29 20:33:19 -0500

    Refactor generateTextStream.ts to add language option and update content guidelines

--- back/src/app/services/ai/generateTextStream.ts
+++ back/src/app/services/ai/generateTextStream.ts
@@ -4,12 +4,14 @@ interface Options {
   prompt: string;
   level: string;
   typeWrite: string;
+  language?: "es" | "en" | "pt"; // ISO 639-1
 }
 
 export const generateTextStreamService = async ({
   prompt,
   level = "A1",
   typeWrite = "Engaging Article",
+  language = "en",
 }: Options) => {
   const openai = new OpenAI({
     apiKey: process.env.OPENAI_API_KEY || "",
@@ -22,20 +24,36 @@ export const generateTextStreamService = async ({
       {
         role: "system",
         content: `
-            Genera un Markdown en INGLES con un estilo de "${typeWrite}"
-            con vocabulario ${level}, el texto tendra
-            # Title
-            ## Subtitle
+          Generate a Markdown text in laguage "${language}" according with ( ISO 639-1 para los idiomas ) , 
+          this text is for an educational purpose with a "${typeWrite}" style, using vocabulary and 
+          complexity appropriate for a ${level} level. 
 
-            Contentent, and if un need to add more subititles it would be use ### Subtitles
+          Formatting guidelines:
+          - Title should be "# Title".
+          - Use one main subtitle as "## Subtitle".
+          - Use additional subtitles as "### Subtitle" if needed.
+          - Avoid using additional H1 or H2 headers after the initial title and subtitle.
 
-            MINIMO 800 characters
-            MAXIMO 2000 characters
-          `,
+          Content guidelines:
+          - Length should be between 2000 and 3000 characters.
+          - For ${level} level:
+            - **A1-A2:** Use simple words, basic sentences, and give short, clear examples. Define complex words as needed.
+            - **B1-B2:** Use intermediate vocabulary, compound sentences, and provide real-world examples.
+            - **C1-C2:** Use advanced vocabulary, complex sentence structures, and offer deeper analysis or insights.
+          - Include quotes or examples to enrich the content where relevant.
+          - Organize content in sections with brief introductions and conclusions.
+
+          Learning aids:
+          - At the end, add 2-3 reflection questions or a short exercise.
+          - For A1-B1 levels, add a glossary of key terms at the end.
+          - For C1-C2 levels, include a brief summary of the main points.
+
+          The markdown should be engaging, informative, and structured to enhance comprehension for readers.
+             `,
       },
       {
         role: "user",
-        content: "The topic would be "+ prompt,
+        content: "The topic would be " + prompt,
       },
     ],
     temperature: 0.8,
