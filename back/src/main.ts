b298b3c9805b57a539a22291863efe8f59890ae2
alexandernovadev <alexsk88.dev@gmail.com>
2024-11-03 14:58:13 -0500

    Refactor version timestamp in main.ts and ProfilePage.tsx

--- back/src/main.ts
+++ back/src/main.ts
@@ -42,7 +42,7 @@ app.use("/", (req, res) => {
   res.json({
     message: "Server is running",
     date: new Date().toISOString(),
-    version: "V. Satudarday 2 November 11:38 PM",
+    version: "V. Satudarday 3 November 3:38 PM",
   });
 });
 
