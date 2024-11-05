9d2938793d8f92f28caddd8f652fa70b78c09079
alexandernovadev <alexsk88.dev@gmail.com>
2024-11-05 18:21:12 -0500

    Refactor Lecture and LectureSchema to include createdAt and updatedAt fields

--- back/src/app/db/models/Lecture.ts
+++ back/src/app/db/models/Lecture.ts
@@ -8,17 +8,24 @@ export interface ILecture extends Document {
   language: string;
   img?: string;
   content: string;
+  createdAt?: Date;
+  updatedAt?: Date;
 }
 
 // Definir el esquema
-const lectureSchema = new Schema<ILecture>({
-  time: { type: Number, required: true },
-  level: { type: String, required: true },
-  typeWrite: { type: String, required: true },
-  language: { type: String, required: true },
-  img: { type: String, default: "" },
-  content: { type: String, required: true },
-});
+const lectureSchema = new Schema<ILecture>(
+  {
+    time: { type: Number, required: true },
+    level: { type: String, required: true },
+    typeWrite: { type: String, required: true },
+    language: { type: String, required: true },
+    img: { type: String, default: "" },
+    content: { type: String, required: true },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 // Crear el modelo
 const Lecture = mongoose.model<ILecture>("Lecture", lectureSchema);
--- front/src/components/pages/Lecture/types/Lecture.ts
+++ front/src/components/pages/Lecture/types/Lecture.ts
@@ -1,9 +1,12 @@
 export interface Lecture {
-  _id: string;
+  _id?: string;
   time: number;
   level: string;
   typeWrite: string;
   language: string;
   img: string;
   content: string;
+  createdAt?: Date; 
+  updatedAt?: Date; 
+  __v?: number;
 }
