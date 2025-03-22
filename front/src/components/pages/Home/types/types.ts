d31d6584a4d72534953a4d38e5184771bb5cf38e
alexandernovadev <alexsk88.dev@gmail.com>
2025-03-22 12:01:07 -0300

    Refactor type definitions in Home component to import LanguagesStr and Levels from centralized types; remove redundant local definitions

--- front/src/components/pages/Home/types/types.ts
+++ front/src/components/pages/Home/types/types.ts
@@ -1,14 +1,11 @@
-export type Language = "English" | "Portugues"; // TODO Esto no debería estar aquí
-export type Level = "A1" | "A2" | "B1" | "B2" | "C1" | "C2";// TODO Esto no debería estar aquí
+import { LanguagesStr, Levels } from "../../../../interfaces/types";
 
 export interface Filters {
-  languages: Language[];
-  levels: Level[];
-}// TODO Esto no debería estar aquí
-
-
+  languages: LanguagesStr[];
+  levels: Levels[];
+}
 export interface Card {
-  _id?: string | number; 
+  _id?: string | number;
   time: number;
   level: string;
   flag?: string;
---
+++ front/src/interfaces/types.ts
@@ -0,0 +1,2 @@
+export type LanguagesStr = "English" | "Portugues"; 
+export type Levels = "A1" | "A2" | "B1" | "B2" | "C1" | "C2";
